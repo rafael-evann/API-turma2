aa9af8ce29c9a259162ed96e261a4116d150eba8
Rafael <rafael.evan.rodrigues.github@gmail.com>
2023-07-17 16:44:14 -0300

    reta final do projeto do figna, adicionando estilização na lista de produtos

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,11 +1,33 @@
-const linkStyles = document.createElement('link');
-linkStyles.setAttribute('rel', 'stylesheet');
-linkStyles.setAttribute('href','./assets/styles/styles.css');
+// Meus estilos
+const reset = document.createElement('link');
+reset.rel = 'stylesheet';
+reset.href = './assets/styles/reset.css';
 
-document.head.appendChild(linkStyles);
+const style = document.createElement('link');
+style.rel = 'stylesheet';
+style.href = './assets/styles/styles.css';
 
-function getProdutos() {
-    
+const print = document.createElement('link');
+print.rel = 'stylesheet';
+print.href = './assets/styles/print.css';
+print.media = 'print';
+// Fontes do Google
+const googleapis = document.createElement('link');
+googleapis.rel = 'preconnect';
+googleapis.href = 'https://fonts.googleapis.com';
+
+const gstatic = document.createElement('link');
+gstatic.rel = 'preconnect';
+gstatic.href = 'https://fonts.gstatic.com';
+gstatic.crossOrigin = '';
+
+const fontUbuntu = document.createElement('link');
+fontUbuntu.rel = 'stylesheet';
+fontUbuntu.href = 'https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap';
+
+document.head.append(reset, style, googleapis, gstatic, fontUbuntu);
+function getProdutos(btnDelete = false) {
+
     fetch('http://localhost:3000/produtos/', {
         method: 'GET',
         headers: {
@@ -15,34 +37,62 @@ function getProdutos() {
         .then(resposta => resposta.json())
         .then(resposta => {
 
-            document.querySelector('#listaProdutos').innerHTML = "Lista de produto";
+            const listaProdutos = document.querySelector('#listaProdutos');
+            listaProdutos.innerHTML = "";
+            listaProdutos.classList.add('lista-produtos');
+
+            const tituloLista = document.createElement('h2');
+            tituloLista.innerHTML = 'Lista de Produtos';
+            tituloLista.classList.add('titulo-lista');
+
+            listaProdutos.append(tituloLista);
 
             for (let i = 0; i < resposta.length; i++) {
 
                 const ul = document.createElement('ul');
                 ul.classList.add('produto');
+                ul.id = `produto-${resposta[i].id}`;
+                ul.tabIndex = '';
 
                 const img = document.createElement('img');
+                img.setAttribute('src', `./assets/images/${resposta[i].imagem}`);
                 img.setAttribute('height', '50');
+                img.setAttribute('data-produto', 'imagem');
 
-                ul.appendChild(document.createElement('li')).innerHTML = resposta[i].id;
+                const liId = document.createElement('li');
+                liId.innerHTML = resposta[i].id;
+                liId.setAttribute('data-produto', 'id');
 
-                ul.appendChild(document.createElement('li')).innerHTML = resposta[i].descricao;
+                const liDescricao = document.createElement('li');
+                liDescricao.innerHTML = resposta[i].descricao;
+                liDescricao.setAttribute('data-produto', 'descricao');
 
-                ul.appendChild(document.createElement('li')).innerHTML = resposta[i].preco;
+                const liPreco = document.createElement('li');
+                liPreco.innerHTML = resposta[i].preco;
+                liPreco.setAttribute('data-produto', 'preco');
 
-                ul.appendChild(document.createElement('li')).appendChild(img).setAttribute('src', resposta[i].imagem);
+                const liImg = document.createElement('li');
+                liImg.appendChild(img);
 
-                document.querySelector('#listaProdutos').appendChild(ul);
+                ul.append(liImg, liId, liDescricao, liPreco);
+
+                // Início do código do botão
+                if (btnDelete) {
+
+                    const liBotao = document.createElement('li');
+
+                    const botao = document.createElement('button');
+                    botao.type = 'button';
+                    botao.innerHTML = '❌';
+                    botao.classList.add('botao-delete');
+                    botao.value = resposta[i].id;
+
+                    ul.appendChild(liBotao).appendChild(botao);
+                }
+                // Fim do código do botão
+
+                listaProdutos.appendChild(ul);
             }
 
         });
 }
-
-
-
-
-
-
-
-
